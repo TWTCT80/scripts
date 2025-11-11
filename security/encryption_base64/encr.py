8aa951940bb4e60d8817330c3c7020db60d50027
Tom Thorsén <tomthorsen@live.se>
2025-11-11 21:10:29 +0100

    added encoder

--- security/encryption_base64/encr.py
+++ security/encryption_base64/encr.py
@@ -1,11 +1,32 @@
-# Simple script that decodes a base64 string
-
+# Simple script that decodes a base64 string, will add some error handling later on
 
 import base64
+
 def decode_pass(password: str):
     decode_bytes = base64.b64decode(password)
     decode_data = decode_bytes.decode()
-    print(f"decoded password {decode_data}")
+    print(f"Decoded password: {decode_data}")
 
-encode_string = input("Enter the base64 string: ")
-decode_pass(encode_string)
+def encode_pass(password: str):
+    coded_bytes = base64.b64encode(password.encode())
+    coded_string = coded_bytes.decode()
+    print(f"Encoded passwd: {coded_string}")
+
+
+choice = 1
+while choice != 9:
+
+    choice = int(input("Enter 1 for encoding or enter 2 for decoding (9 to quit): "))
+
+    if choice == 1:
+        kod = input("Enter text to code: ")
+        encode_pass(kod)
+    elif choice == 2:
+        encode_string = input("Enter the base64 string: ")
+        decode_pass(encode_string)
+    
+    elif choice == 9:
+        print("Bye!")
+        break
+    else:
+        print("Try again")
