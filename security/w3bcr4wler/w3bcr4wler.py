890c3f888886b93ef280c06cd10d178d30d77aad
Tom Thorsén <tomthorsen@live.se>
2025-12-03 16:37:28 +0100

    w3bcr4wler 1st commit

---
+++ security/w3bcr4wler/w3bcr4wler.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Jag använder alltså requests för att hämta sidan, 
+# och BeautifulSoup för att analysera innehållet.
+
+def get_page(url):
+    pass
+
+
+print(r"""
+      
+        /\  .-"^"-.  /\
+       //\\/  ,,,  \//\\
+       |/\| ,;;;;;, |/\|
+       //\\\;-"-"-;///\\
+      //  \/   .   \/  \\
+     (| ,-_| \ | / |_-, |)
+       //`__\.-.-./__`\\
+      // /.-(() ())-.\ \\
+     (\ |)   '---'   (| /)
+      ` (|           |) `
+        \)           (/
+    -----------------------
+     ---- W3BCR4WLER ----
+""")
+get_page(input("What URL would you like to cr4wl over: "))
